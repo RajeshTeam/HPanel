4b72d568bfee827e010532d942018207a6184146
MrBoombastic <18630357+MrBoombastic@users.noreply.github.com>
2020-10-17 17:04:40 +0200

    poprawka korony

--- tools.js
+++ tools.js
@@ -9,43 +9,55 @@ const client = new Twitter({        //definiujemy klienta Twittera, pobieranie i
 });
 module.exports = {
     refreshMZGOV: function () {
-        let stats = {};
-        const params = {screen_name: 'MZ_GOV_PL', count: 10, tweet_mode: "extended"}; //nie można na żywo pobierać danych z Twitterka, więc pobieramy co jakiś czas 10 ostatnich
-        client.get('statuses/user_timeline', params, function (error, tweets) {
-            if (!error) {
-                let next = false;
-                tweets.reverse();
-                tweets.forEach(t => {
-                    if (t.full_text.includes("Mamy ") && !skippedID.includes(t.id)) {
-                        stats.content = [];
-                        stats.content.push(t.full_text);
-                        stats.id = t.id_str;
-                        stats.time = moment(t.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY').locale("pl").format('D MMMM YYYY, HH:mm');
-                        stats.lastTime = moment().locale("pl").format('D MMMM YYYY, HH:mm');
-                        skippedID.push(t.id);
-                        return next = true;
-                    }
-                    if (next) {
-                        stats.content.push(t.full_text);
-                        skippedID.push(t.id);
-                        if (t.full_text.includes("Liczba zakażonych koronawirusem:")) //koniec zbierania twitów
-                        {
-                            next = false;
-                            stats.size = stats.content[0]?.match(new RegExp("Mamy " + "(.*)" + " nowych"))[1].replace(/ /g, '');
-                            stats.finalContent = stats.content?.join(' ').replace(/\n/g, ' ');
-                            fs.writeFileSync("./data/corona.json", JSON.stringify(stats));
+        try {
+            let stats = {};
+            const params = {screen_name: 'MZ_GOV_PL', count: 10, tweet_mode: "extended"}; //nie można na żywo pobierać danych z Twitterka, więc pobieramy co jakiś czas 10 ostatnich
+            client.get('statuses/user_timeline', params, function (error, tweets) {
+                if (!error) {
+                    let next = false;
+                    tweets.reverse();
+                    tweets.forEach(t => {
+                        if (t.full_text.includes("Mamy ") && !skippedID.includes(t.id)) {
+                            stats.content = [];
+                            stats.content.push(t.full_text);
+                            stats.id = t.id_str;
+                            stats.time = moment(t.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY').locale("pl").format('D MMMM YYYY, HH:mm');
+                            stats.lastTime = moment().locale("pl").format('D MMMM YYYY, HH:mm');
+                            skippedID.push(t.id);
+                            return next = true;
                         }
-                    }
-                });
-            } else console.log(error);
-        });
+                        if (next) {
+                            stats.content.push(t.full_text);
+                            skippedID.push(t.id);
+                            if (t.full_text.includes("Liczba zakażonych koronawirusem:")) //koniec zbierania twitów
+                            {
+                                next = false;
+                                stats.size = stats.content[0]?.match(new RegExp("Mamy " + "(.*)" + " now"))?.[1].replace(/ /g, '');
+                                stats.finalContent = stats.content?.join(' ').replace(/\n/g, ' ');
+                                fs.writeFileSync("./data/corona.json", JSON.stringify(stats));
+                            }
+                        }
+                    });
+                } else console.log(error);
+            });
+        } catch (e) {
+            console.log(e);
+        }
     },
     probeRefreshMZGOV: function () {
-        const params = {screen_name: 'MZ_GOV_PL', count: 1, tweet_mode: "extended"}; //jeżeli ostatni twitter jest nowy, to odświeżamy całość
-        client.get('statuses/user_timeline', params, function (error, tweets) {
-            if (!error) {
-                if (!skippedID.includes(tweets[0].id)) require("./tools").refreshMZGOV();
-            }
-        });
+        try {
+            const params = {screen_name: 'MZ_GOV_PL', count: 1, tweet_mode: "extended"}; //jeżeli ostatni twitter jest nowy, to odświeżamy całość
+            client.get('statuses/user_timeline', params, async function (error, tweets) {
+                if (!error) {
+                    const stats = JSON.parse(await fs.readFileSync("./data/corona.json"));
+                    stats.lastTime = moment().locale("pl").format('D MMMM YYYY, HH:mm');
+                    await fs.writeFileSync("./data/corona.json", JSON.stringify(stats));
+                    if (!skippedID.includes(tweets[0].id)) require("./tools").refreshMZGOV();
+                }
+            });
+
+        } catch (e) {
+            console.log(e);
+        }
     }
 };
